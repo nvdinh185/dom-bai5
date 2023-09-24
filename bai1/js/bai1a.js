e789dc3e01cda61775d667eb12b8a98bce12b2c7
nvdinh185 <nvdinh0766777123@gmail.com>
2023-09-24 15:12:42 +0700

    xx

--- bai1/js/bai1a.js
+++ bai1/js/bai1a.js
@@ -14,9 +14,6 @@ function handleBlurInput(input) {
             $(errorElement).attr('style', 'color: red; font-style: italic;');
             $(errorElement).text('Vui lòng nhập');
             input.addClass('invalid');
-        } else {
-            $(errorElement).text('');
-            input.removeClass('invalid');
         }
     })
 
@@ -38,21 +35,38 @@ form.on("submit", function (e) {
             formValue[el.name] = el.value;
         }
     }
-    var chieuDai = Number(formValue['chieudai']);
-    var chieuRong = Number(formValue['chieurong']);
 
     var resultElement = $('#result');
-    if (chieuDai && chieuRong) {
+    var check = true;
+    if (!formValue['chieudai']) {
+        isRequired(chieuDaiElement);
+        check = false;
+    }
+    if (!formValue['chieurong']) {
+        isRequired(chieuRongElement);
+        check = false;
+    }
+    if (check) {
+        var chieuDai = Number(formValue['chieudai']);
+        var chieuRong = Number(formValue['chieurong']);
         var chuVi = (chieuDai + chieuRong) * 2;
         var dienTich = chieuDai * chieuRong;
         resultElement.html(`
             <p>Chu vi: ${chuVi}</p>
             <p>Diện tích: ${dienTich}</p>
         `);
-    } else {
-        resultElement.html(`
-            <p style="color: red">Vui lòng nhập đầy đủ thông tin vào!</p>
-        `);
+    }
+
+    function isRequired(input) {
+        var errorElement = input.parent().children()[2];
+        if (input.val().trim() === '') {
+            Object.assign(errorElement.style, {
+                color: 'red',
+                fontStyle: 'italic'
+            })
+            $(errorElement).text('Yêu cầu nhập!');
+            input.addClass('invalid');
+        }
     }
 
 })
